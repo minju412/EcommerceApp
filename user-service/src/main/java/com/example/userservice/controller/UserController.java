90784b632ad994ac3bd59dbe1d3f06fd10f77810
minju412 <ln8847@ajou.ac.kr>
2022-09-29 01:30:23 +0900

    feat: welcome() 메서드 추가

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.example.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.userservice.vo.Greeting;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
+    @Autowired // 이처럼 직접 주입 받는 것은 권장하지 않는다.
+    private Greeting greeting;
+
+    private Environment env;
+
+    @Autowired
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("health-check")
     public String status() {
         return "It's Working in User Service";
+    }
+
+    @GetMapping("welcome")
+    public String welcome() {
+        // return env.getProperty("greeting.message");
+        return greeting.getMessage();
     }
 }
