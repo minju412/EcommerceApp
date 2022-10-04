68b3109705e4fe3ef115bc339a23626fe1f3d95f
minju412 <ln8847@ajou.ac.kr>
2022-10-04 19:03:25 +0900

    gateway-service Actuator 설정

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,13 +1,24 @@
 package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    /**
+     * httptrace 를 사용하기 위해 bean 등록
+     */
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
